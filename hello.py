db4597be65a96e9c7ab9d683109f1b283c4a56b1
yghonsikar <119158099+yghonsikar@users.noreply.github.com>
2024-03-13 11:48:09 +0530

    This is my First Commit for the Jenkins_lab
    Here I am just checking the  Gitlab concepts

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("hello Yash i am python")
+list=["fcgv","gfgh","kjhbnm"]
+print(list)
+list[0]="jvcdsrdfghjkbvcdxtfcghjjhcc"
+list.append("San")
+list.remove("jvcdsrdfghjkbvcdxtfcghjjhcc")
+print(list)
+info_list= {
+    "name" : "yash",
+    "id" : 21,
+    "org" : "Admin"
+}
+print(info_list)
+info_list["Salary"]= "Not defined"
+print(info_list)
+
+my_set={1,2,3,4,5,56,6,1,24,6,12,44,5,11,2,3,4,5}
+print(my_set)
